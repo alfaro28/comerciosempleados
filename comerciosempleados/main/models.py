07d08fcfbb5ca0964d44710b4c37b09eb194d897
Rodrigo Rivas <rodrigo.rivas@dapp.mx>
2021-07-23 17:08:05 -0500

    modelos y serializers

--- comerciosempleados/main/models.py
+++ comerciosempleados/main/models.py
@@ -1,3 +1,29 @@
+import uuid as _uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Comercio(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    uuid = models.UUIDField(default=_uuid.uuid4, editable=False)
+    nombre = models.CharField(max_length=100)
+    activo = models.BooleanField(default=True)
+    email_contacto = models.EmailField(max_length=50, null=True, blank=True)
+    telefono_contacto = models.CharField(max_length=15, null=True, blank=True)
+    api_key = models.UUIDField(default=_uuid.uuid4, editable=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+
+class Empleado(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    uuid = models.UUIDField(default=_uuid.uuid4, editable=False)
+    nombre = models.CharField(max_length=40)
+    apellidos = models.CharField(max_length=40)
+    pin = models.CharField(max_length=6)
+    comercio = models.ForeignKey('main.Comercio', related_name='empleados',
+                                 on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    activo = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('pin', 'comercio', )
--- comerciosempleados/main/serializers.py
+++ comerciosempleados/main/serializers.py
@@ -1,1 +1,15 @@
 from rest_framework import serializers
+
+from comerciosempleados.main.models import Empleado
+
+
+class EmpleadoSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(source="uuid")
+    nombre_completo = serializers.SerializerMethodField()
+
+    def get_nombre_completo(self, obj):
+        return obj.nombre + " " + obj.apellidos
+
+    class Meta:
+        model = Empleado
+        fields = ('id', 'nombre_completo', 'pin', 'fecha_creacion', 'activo')
